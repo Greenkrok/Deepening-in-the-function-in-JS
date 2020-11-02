36c90493a9c604cb28a9dd702d3a1c05b8713d40
Greenkrok <hellsing_13@mail.ru>
2020-11-03 00:28:40 +0300

    feat: add task-3

--- scripts/task-3.js
+++ scripts/task-3.js
@@ -6,3 +6,22 @@
 // Прим на входе [“пирамида”, “юг”, “параллелограмм”] на выходе [8, 2, 14]
 // Принимает массив строк, выводит их в виде строки, разделенной запятыми.
 
+function Strings(mass) {
+    this.elLenght = function() {
+        return mass.length;
+    }
+    this.massLenght = function() {
+        let massLength = mass.map(function(el) {
+            return el.length;
+          });
+          return massLength;
+    }
+    this.joinMass = function() {
+        return mass.join(', ');
+    }
+}
+
+var str = new Strings(['пирамида', 'юг', 'параллелограмм']);
+console.log(str.elLenght());
+console.log(str.massLenght());
+console.log(str.joinMass());
